e1ac910ce82c166024ad097e1e78db221bd05626
sweetguma <sooensible@gmail.com>
2026-09-14 20:32:17 +0200

    Config: add DB_DIR, TRAINED_MODEL_PATH, SHARED_EMBEDDING_MODEL

--- src/ycis/config.py
+++ src/ycis/config.py
@@ -2,6 +2,8 @@ import os
 from dataclasses import dataclass
 from pathlib import Path
 from dotenv import load_dotenv
+from langchain_huggingface import HuggingFaceEmbeddings
+from huggingface_hub import snapshot_download
 
 # .env load
 load_dotenv()
@@ -10,6 +12,7 @@ load_dotenv()
 class Config:
     PROJECT_ROOT: Path = Path(__file__).resolve().parents[2]
     DATA_DIR: Path = PROJECT_ROOT / "data"
+    DB_DIR: Path = DATA_DIR / "DB"
     JSON_DIR: Path = DATA_DIR / "json"
 
     # APIs
@@ -22,7 +25,11 @@ class Config:
 
     # Inference setting
     TRAINED_MODEL_ID = "sweetguma/bert-sentiment-model"
+    TRAINED_MODEL_PATH = snapshot_download(repo_id=TRAINED_MODEL_ID)
     BATCH_SIZE: int = 32
     MAX_LENGTH: int = 128 # comment max length
+    SHARED_EMBEDDING_MODEL = HuggingFaceEmbeddings(
+    model_name="all-MiniLM-L6-v2", model_kwargs={"device": "cpu"}
+)
 
 config = Config()
